29fc187ec67c4290bef486925b7fd6a71ed26a62
Leopoldo <leopoldonm@outlook.com>
2023-07-28 19:53:44 -0700

    Complete: Completed groups Routes
    Route has been completed and tested.

--- routes/groups.routes.js
+++ routes/groups.routes.js
@@ -1,67 +1,128 @@
 //Imports
 const router = require("express").Router();
+const {Types: {ObjectId}} = require('mongoose');
 
 //Import models
 const Groups = require("../models/group.model");
+const Posts = require("../models/post.model");
+
+// Import Middleware
+const isLoggedIn = require("../middleware/isLoggedIn");
+
+//Functions
+const isValidObjectId = (id) => ObjectId.isValid(id) && (new ObjectId(id)).toString() === id;
 
 //Create a group
-router.post("/", (req, res) => {
-  Groups.create(req.body)
-    .then((group) => {
-      res.json(group);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ message: "Internal Server Error" });
-    });
+router.post("/", isLoggedIn, (req, res) => {
+    //Destructuring constants
+    const {groupDescription, groupName} = req.body;
+    const user = req.user._id;
+
+    // Validate data
+    if (typeof groupDescription != 'string') return res.status(400).json({errorMessage: 'Please provide a group description'});
+    if (typeof groupName != 'string') return res.status(400).json({errorMessage: 'Please provide a group name'});
+
+    // Create a group object
+    const newGroup = {user, groupName, groupDescription};
+
+    // Create the Group
+    Groups.create(newGroup)
+        .then((group) => {
+            return res.json(group);
+        })
+        .catch((err) => {
+            return res.status(400).json({errorMessage: err.message});
+        });
 });
 
-//Read all Groups
+//Read all groups
 router.get("/", (req, res) => {
-  Groups.find()
-    .then((groups) => {
-      res.json(groups);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ message: "Internal Server Error" });
-    });
+    Groups.find()
+        .then((groups) => {
+            return res.json(groups);
+        })
+        .catch((err) => {
+            return res.status(400).json({errorMessage: err.message});
+        });
 });
 
-//Read an Issue
-router.get("/:groupId", (req, res) => {
-  Groups.findById(req.params.groupId)
-    .then((group) => {
-      res.json(group);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ message: "Internal Server Error" });
-    });
+//Read a single group
+router.get("/:groupId", async (req, res) => {
+    // Validate the group ID
+    if (!isValidObjectId(req.params.groupId)) return res.status(404).json({errorMessage: "Group ID is incorrect"});
+
+    // Retrieve the group
+    const groupInfo = await Groups.findById(req.params.groupId)
+        .catch((err) => {
+            return res.status(400).json({errorMessage: err.message});
+        });
+
+    // Returns error if a group doesn't exist
+    if (!groupInfo) return res.status(404).json({errorMessage: "Group doesn't exist"});
+
+    // Retrieves all posts from that event
+    const postInfo = await Posts.find({groupId: ObjectId(req.params.groupId)})
+        .catch((err) => {
+            return res.status(400).json({errorMessage: err.message});
+        });
+
+    return res.json({groupInfo, postInfo});
 });
 
-//Updates a group
-router.patch("/:groupId", (req, res) => {
-  Groups.findByIdAndUpdate(req.params.groupId, req.body, { new: true })
-    .then((group) => {
-      res.json(group);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ message: "Internal Server Error" });
-    });
+//Update a group
+router.patch("/:groupId", isLoggedIn, async (req, res) => {
+    // Validate the group ID
+    if (!isValidObjectId(req.params.groupId)) return res.status(400).json({errorMessage: "Group ID is incorrect."});
+
+    // Retrieve group
+    const groupInfo = await Groups.findById(req.params.groupId).populate({path: "user", model: 'User'})
+        .catch((err) => {
+            return res.status(400).json({errorMessage: err.message});
+        });
+
+    // Validate that the group exists and the user is the owner.
+    if (!groupInfo) return res.status(404).json({errorMessage: "Group doesn't exist"});
+    if (groupInfo["user"].username !== req.user.username) return res.status(401).json({errorMessage: 'Not authorized'});
+
+    // Destructure the request
+    const {groupName, groupDescription} = req.body;
+
+    // Field validation
+    if (typeof groupDescription != 'string') return res.status(400).json({errorMessage: 'Please provide a group description'});
+    if (typeof groupName != 'string') return res.status(400).json({errorMessage: 'Please provide a group name'});
+
+    // Create a group object
+    const updateGroup = {groupDescription, groupName};
+
+    // Edit the group
+    Groups.findByIdAndUpdate(req.params.groupId, updateGroup, {new: true})
+        .then((group) => {
+            return res.json(group);
+        })
+        .catch((err) => {
+            return res.status(400).json({errorMessage: err.message});
+        });
 });
 
 //Deletes a group
-router.delete("/:groupId", (req, res) => {
-  Groups.findByIdAndDelete(req.params.groupId)
-    .then((group) => {
-      res.json(group);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ message: "Internal Server Error" });
-    });
+router.delete("/:groupId", isLoggedIn, async (req, res) => {
+    // Validate the group ID
+    if (!isValidObjectId(req.params.groupId)) return res.status(404).json({errorMessage: "Group ID is incorrect"});
+
+    // Retrieve group
+    const group = await Groups.findById(req.params.groupId).populate({path: 'user', model: 'User'});
+
+    // Validate that the group exists and the user is the owner.
+    if (!group) return res.status(404).json({errorMessage: "Group doesn't exist"});
+    if (group['user'].username !== req.user.username) return res.status(401).json({errorMessage: 'Not Authorized'});
+
+    Groups.findByIdAndDelete(req.params.groupId)
+        .then((group) => {
+            return res.json(group);
+        })
+        .catch((err) => {
+            return res.status(400).json({errorMessage: err.message});
+        });
 });
 
 module.exports = router;
