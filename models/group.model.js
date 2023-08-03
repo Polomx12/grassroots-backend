80b2d9c0c6c6fc54294b36c82248fae38bc3a608
Leopoldo <leopoldonm@outlook.com>
2023-08-02 20:08:52 -0700

    CleanUp: Models Updated
    Models were updated for comprehension and for consistency with routes

--- models/group.model.js
+++ models/group.model.js
@@ -1,24 +1,28 @@
-const { Schema, model } = require("mongoose");
+const {Schema, model} = require("mongoose");
 
 const GroupSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: [true, 'User is required.']
-  },
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: [true, 'User is required.']
+        },
 
-  groupDescription: {
-    type: String,
-    required: [true, 'Group description is required.'],
-      maxLength: 300
-  },
+        groupDescription: {
+            type: String,
+            required: [true, 'Group description is required.'],
+            maxLength: 300
+        },
 
-  groupName: {
-    type: String,
-    required: [true, "Group Name is required"],
-      maxLength: 100
-  },
-});
+        groupName: {
+            type: String,
+            required: [true, "Group Name is required"],
+            maxLength: 100
+        },
+    },
+    {
+        timestamps: true
+    }
+);
 
 const Group = model("Group", GroupSchema);
 
