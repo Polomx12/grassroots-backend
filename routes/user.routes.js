69db7e2f00b68379807da2f6c402d11f0d8892fa
Polomx12 <leopoldonm@outlook.com>
2023-09-01 02:21:19 +0000

    WIP: Added delete functionality for user accounts.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,15 @@
-//Imports
+// Imports
 const router = require("express").Router();
+const {Types: {ObjectId}} =  require("mongoose");
 
-//Import models
+// Import models
 const User = require("../models/user.model");
+
+//Import middleware
+const isLoggedIn = require("../middleware/isLoggedIn");
+
+// Functions
+const isValidObjectId = (id) => ObjectId.isValid(id) && (new Object(id)).toString() === id;
 
 //Read Ids
 router.get("/:id", (req, res) => {
@@ -24,6 +31,30 @@ router.post("/:id", (req, res) => {
         })
         .catch((e) => {
             console.log(e);
+        });
+});
+
+// Deletes a user
+router.delete('/:userId', isLoggedIn, async (req, res)=>{
+    if (!isValidObjectId(req.params.userId)) return res.status(404).json({errorMessage: "User ID is incorrect"});
+
+    // Retrieve the user
+    const user = await User.findById(req.params.userId)
+        .catch((err)=>{
+            return res.status(400).json({errorMessage: err.message});
+        });
+
+    // Validate that the user exists and it is the user requesting the deletion.
+    if (!user) return res.status(404).json({errorMessage: "User doesn't exist"});
+    if (user.username !== req.user.username) return res.status(401).json({errorMessage:"Not Authorized"});
+
+    // Delete the user
+    User.findByIdAndDelete(req.params.userId)
+        .then((a) => {
+            return res.status(200).json("Account deleted.")
+        })
+        .catch((err)=>{
+            return res.status(400).json({errorMessage: err.mesage});
         });
 });
 
