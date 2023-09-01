7c7f59905a0f78e5d585491633d6cdd1a40ced3e
Polomx12 <leopoldonm@outlook.com>
2023-09-01 02:20:35 +0000

    Fixed CORS issue so that it works on sandbox

--- config/server.config.js
+++ config/server.config.js
@@ -14,8 +14,15 @@ module.exports = (app) => {
     cors({
       credentials: true,
       origin: "*",
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
     })
   );
+
+  app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
   // During development this will log all of our requests.
   app.use(logger("dev"));
