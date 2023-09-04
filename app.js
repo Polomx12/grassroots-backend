6d048aed201cd5bad819f276b044899e2270fea0
Polomx12 <leopoldonm@outlook.com>
2023-09-04 20:01:29 +0000

    SetUp: Cloudinary configuration will run with server.

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ require("dotenv/config");
 //This connects us to the MongoDB database
 require("./config/mongoAtlas.config");
 
+// This connects us to Cloduinary
+require("./config/cloudinary.config");
+
 //Express handles our HTTP requests and initializes our application
 const express = require("express");
 
